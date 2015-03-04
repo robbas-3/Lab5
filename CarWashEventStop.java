b84b3faaf143923bb792b9fe6e9decb74b0a8387
robbas-3 <robin.bassili@hotmail.com>
2015-03-04 15:48:10 +0100

    Update CarWashEventStop.java

--- CarWashEventStop.java
+++ CarWashEventStop.java
@@ -6,12 +6,19 @@ public class CarWashEventStop extends Event {
 	}
 	
 	/**
-	 * Stop-eventet sätter statets stopvariabel till sann så exekveringen av
-	 * simuleringen slutar vid nästa koll efter event.
+	 * Stop-eventet sÃ¤tter statets stopvariabel till sann sÃ¥ exekveringen av
+	 * simuleringen slutar vid nÃ¤sta koll efter event.
 	 * Sedan printar den ut slut info (se lab 5 PDF:en).
 	 */
 	public void execute(Simulator sim, SimState ss) {
 		ss.stop = true;
+		
+		System.out.println("-----------------------------------------");
+		System.out.println("Total idle machine time: " + ss.idleTime);
+		System.out.println("Total queueing time: " + ss.time);
+		System.out.println("Mean queueing time: " + ((ss.time)/(ss.counter)));
+		System.out.println("Rejected cars: " + ss.rejectedCars);
+		
 	}
 	
 	public String toString() {
